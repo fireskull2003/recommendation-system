96580df42cc9e7a3ee73f278160ea3510cebf29c
Rohit kumar <102460696+fireskull2003@users.noreply.github.com>
2024-05-03 17:03:01 +0530

    Add files via upload

---
+++ recommendation_engine.py
@@ -0,0 +1,231 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+from matplotlib import pyplot as plt
+import warnings
+import os
+import importlib
+warnings.filterwarnings('ignore')
+sns.set_theme(color_codes=True)
+
+# user-defined function to check library is installed or not, if not installed then it will install automatically at runtime.
+def check_and_install_library(library_name):
+    try:
+        importlib.import_module(library_name)
+        print(f"{library_name} is already installed.")
+    except ImportError:
+        print(f"{library_name} is not installed. Installing...")
+        try:
+            import pip
+            pip.main(['install', library_name])
+        except:
+            print("Error: Failed to install the library. Please install it manually.")
+
+"""The Amazon product review dataset is huge, size of the dataset is 320 MB so it's recommended to download using the Kaggle repository which will be handy for further execution and will save your time and resources."""
+
+if 'amazon-product-reviews' not in os.listdir():
+  check_and_install_library('opendatasets')
+  import opendatasets as od
+  od.download('https://www.kaggle.com/datasets/irvifa/amazon-product-reviews')
+
+#load the dataframe and set column name
+df=pd.read_csv('amazon-product-reviews/ratings_Electronics.csv',names=['userId', 'productId','rating','timestamp'])
+print(df)
+
+df.shape
+
+df.columns
+
+"""Since our dataset is too big and it will be difficult to analyze the entire dataset due to limited resources,thats'why I am randomly taking 20% of the data as sample out of the whole dataset which is 1564896."""
+
+electronics_data=df.sample(n=1564896,ignore_index=True)
+electronics_data
+
+#after taking samples drop df to release the memory occupied by entire dataframe
+del df
+
+#print top 5 records of the dataset
+electronics_data.head()
+
+#print the concise information of the dataset
+electronics_data.info()
+
+#drop timestamp column
+electronics_data.drop('timestamp',axis=1,inplace=True)
+
+electronics_data.describe()
+
+"""As we can see minimum rating of the products is 1 , maximum rating is 5 and average rating of the products is around 4."""
+
+#handle missing values
+electronics_data.isnull().sum()
+
+#handling duplicate records
+electronics_data[electronics_data.duplicated()].shape[0]
+
+electronics_data.head()
+
+plt.figure(figsize=(8,4))
+sns.countplot(x='rating',data=electronics_data)
+plt.title('Rating Distribution')
+plt.xlabel('Rating')
+plt.ylabel('Count')
+plt.grid()
+plt.show()
+
+"""most of the user have given 5 rating"""
+
+print('Total rating : ',electronics_data.shape[0])
+print('Total unique users : ',electronics_data['userId'].unique().shape[0])
+print('Total unique products : ',electronics_data['productId'].unique().shape[0])
+
+"""analyzing the rating by user"""
+
+no_of_rated_products_per_user = electronics_data.groupby(by='userId')['rating'].count().sort_values(ascending=False)
+no_of_rated_products_per_user.head()
+
+print('No of rated product more than 50 per user : {} '.format(sum(no_of_rated_products_per_user >= 50)))
+
+"""**Popularity Based Recommendation**
+
+Popularity based recommendation system works with the trend. It basically uses the items which are in trend right now. For example, if any product which is usually bought by every new user then there are chances that it may suggest that item to the user who just signed up.
+
+The problems with popularity based recommendation system is that the personalization is not available with this method i.e. even though you know the behaviour of the user but you cannot recommend items accordingly.
+"""
+
+data=electronics_data.groupby('productId').filter(lambda x:x['rating'].count()>=50)
+
+data.head()
+
+no_of_rating_per_product=data.groupby('productId')['rating'].count().sort_values(ascending=False)
+no_of_rating_per_product
+
+no_of_rating_per_product.head()
+
+#top 20 product
+no_of_rating_per_product.head(20).plot(kind='bar')
+plt.xlabel('Product ID')
+plt.ylabel('num of rating')
+plt.title('top 20 procduct')
+plt.show()
+
+#average rating product
+mean_rating_product_count=pd.DataFrame(data.groupby('productId')['rating'].mean())
+
+mean_rating_product_count.head()
+
+#plot the rating distribution of average rating product
+plt.hist(mean_rating_product_count['rating'],bins=100)
+plt.title('Mean Rating distribution')
+plt.show()
+
+#check the skewness of the mean rating data
+mean_rating_product_count['rating'].skew()
+
+#it is highly negative skewed
+
+mean_rating_product_count['rating_counts'] = pd.DataFrame(data.groupby('productId')['rating'].count())
+
+mean_rating_product_count.head()
+
+#highest mean rating product
+mean_rating_product_count[mean_rating_product_count['rating_counts']==mean_rating_product_count['rating_counts'].max()]
+
+#min mean rating product
+print('min average rating product : ',mean_rating_product_count['rating_counts'].min())
+print('total min average rating products : ',mean_rating_product_count[mean_rating_product_count['rating_counts']==mean_rating_product_count['rating_counts'].min()].shape[0])
+
+#plot the rating count of mean_rating_product_count
+plt.hist(mean_rating_product_count['rating_counts'],bins=100)
+plt.title('rating count distribution')
+plt.show()
+
+#joint plot of rating and rating counts
+sns.jointplot(x='rating',y='rating_counts',data=mean_rating_product_count)
+plt.title('Joint Plot of rating and rating counts')
+plt.tight_layout()
+plt.show()
+
+plt.scatter(x=mean_rating_product_count['rating'],y=mean_rating_product_count['rating_counts'])
+plt.show()
+
+print('Correlation between Rating and Rating Counts is : {} '.format(mean_rating_product_count['rating'].corr(mean_rating_product_count['rating_counts'])))
+
+"""**Collaberative filtering (Item-Item recommedation)**
+
+Collaborative filtering is commonly used for recommender systems. These techniques aim to fill in the missing entries of a user-item association matrix. We are going to use collaborative filtering (CF) approach. CF is based on the idea that the best recommendations come from people who have similar tastes. In other words, it uses historical item ratings of like-minded people to predict how someone would rate an item.Collaborative filtering has two sub-categories that are generally called memory based and model-based approaches.
+"""
+
+#import surprise library for collebrative filtering
+check_and_install_library('surprise')
+from surprise import KNNWithMeans
+from surprise import Dataset
+from surprise import accuracy
+from surprise import Reader
+from surprise.model_selection import train_test_split
+
+#Reading the dataset
+reader = Reader(rating_scale=(1, 5))
+surprise_data = Dataset.load_from_df(data,reader)
+
+#Splitting surprise the dataset into 80,20 ratio using train_test_split
+trainset, testset = train_test_split(surprise_data, test_size=0.3,random_state=42)
+
+# Use user_based true/false to switch between user-based or item-based collaborative filtering
+algo = KNNWithMeans(k=5, sim_options={'name': 'pearson_baseline', 'user_based': False})
+algo.fit(trainset)
+
+#make prediction using testset
+test_pred=algo.test(testset)
+
+#print RMSE
+print("Item-based Model : Test Set")
+accuracy.rmse(test_pred ,verbose=True)
+
+"""**Model-based collaborative filtering system**
+
+These methods are based on machine learning and data mining techniques. The goal is to train models to be able to make predictions. For example, we could use existing user-item interactions to train a model to predict the top-5 items that a user might like the most. One advantage of these methods is that they are able to recommend a larger number of items to a larger number of users, compared to other methods like memory based approach. They have large coverage, even when working with large sparse matrices.
+"""
+
+data2=data.sample(20000)
+ratings_matrix = data2.pivot_table(values='rating', index='userId', columns='productId', fill_value=0)
+ratings_matrix.head()
+
+"""above we have created a pivot table of the rating where the value of the column will rating and index will be userId and the column name will be productId"""
+
+#check the shape of the rating_matrix
+ratings_matrix.shape
+
+#transpose the metrix to make column (productId) as index and index as column (userId)
+x_ratings_matrix=ratings_matrix.T
+x_ratings_matrix.head()
+
+x_ratings_matrix.shape
+
+#Decomposition of the matrix using Singular Value Decomposition technique
+from sklearn.decomposition import TruncatedSVD
+SVD = TruncatedSVD(n_components=10)
+decomposed_matrix = SVD.fit_transform(x_ratings_matrix)
+decomposed_matrix.shape
+
+#Correlation Matrix
+correlation_matrix = np.corrcoef(decomposed_matrix)
+correlation_matrix.shape
+
+x_ratings_matrix.index[10]
+
+i="B00001P4ZH"
+product_names=list(x_ratings_matrix.index)
+product_id=product_names.index(i)
+print(product_id)
+
+"""Correlation for all items with the item purchased by this customer based on items rated by other customers people who bought the same product"""
+
+correlation_product_ID = correlation_matrix[product_id]
+correlation_product_ID.shape
+
+correlation_matrix[correlation_product_ID>0.75].shape
+
+#Recommending top 20 highly correlated products in sequence
+recommend = list(x_ratings_matrix.index[correlation_product_ID > 0.75])
+recommend[:20]
